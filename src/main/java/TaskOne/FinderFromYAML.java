db231e59b48210acbbc74771952da151126205a1
Vladislav <bukatinvladislav@gmail.com>
2015-04-16 22:21:24 +0300

    TaskOne

---
+++ src/main/java/TaskOne/FinderFromYAML.java
@@ -0,0 +1,28 @@
+package TaskOne;
+
+import org.ho.yaml.YamlDecoder;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Created by Vlad on 16.04.2015.
+ */
+public class FinderFromYAML {
+    public static void main (String [] args) throws IOException {
+        File file = new File("D://SysGearsTest.txt");
+        InputStream inp = new FileInputStream(file);
+        YamlDecoder decoder = new YamlDecoder(inp);
+
+        Integer[] obj = decoder.readObjectOfType(Integer[].class);
+        int result=0;
+        //Since we have only one single number, we will approach the operation XOR, because x ^ x = 0, a 0 ^ y = y.
+        for (int i = 0; i < obj.length; i++) {
+            result = result ^ obj[i];
+         }
+        System.out.println(result);
+        decoder.close();
+        inp.close();
+    }
+}
+
---
+++ src/main/java/TaskOne/GenerateYAML.java
@@ -0,0 +1,57 @@
+package TaskOne;
+
+import org.ho.yaml.YamlEncoder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by Vlad on 16.04.2015.
+ */
+public class GenerateYAML {
+    public static void main(String[] args) throws IOException {
+
+        // Generate array
+        //Sets the range for the random number
+        int min = 0;
+        int maxCountOfNumbers = 1000000;
+        int max = Integer.MAX_VALUE / 2;
+
+        int countOfNumbers = min + (int) (Math.random() * ((maxCountOfNumbers - min) + 1));
+        // We creat array with even count of Numbers. All numbers have a pair. then we remove one.
+        if (countOfNumbers % 2 != 0) {
+            countOfNumbers = countOfNumbers + 1;
+        }
+        System.out.println(countOfNumbers);
+        // I need to remove one element, so i use ArrayList, not array.
+        ArrayList<Integer> array = new ArrayList<Integer>(countOfNumbers);
+        //fill an array with random numbers
+        for (int i = 0; i < countOfNumbers; i = i + 2) {
+            int newNumber = min + (int) (Math.random() * ((max - min) + 1));
+            array.add(newNumber);
+            int position = 0 + (int) (Math.random() * (((array.size() - 1) - 0) + 1));
+            array.add(position, newNumber);
+        }
+        //remove random element
+        int positionForRemove = 0 + (int) (Math.random() * (((array.size() - 1) - 0) + 1));
+        array.remove(positionForRemove);
+        // create array
+        int[] arrayToFile = new int[countOfNumbers - 1];
+        int contIter = 0;
+        for (Integer res : array) {
+            arrayToFile[contIter++] = res.intValue();
+        }
+        File file = new File("D://SysGearsTest.txt");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        OutputStream out = new FileOutputStream(file);
+        YamlEncoder enc = new YamlEncoder(out);
+        enc.writeObject(arrayToFile);
+        enc.close();
+        out.close();
+    }
+}

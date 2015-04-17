bf59eb684398eae38e101dacdb8e713878da0a7c
Vladislav <bukatinvladislav@gmail.com>
2015-04-17 10:33:58 +0300

    TaskOne

--- src/main/java/TaskOne/GenerateYAML.java
+++ src/main/java/TaskOne/GenerateYAML.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Vlad on 16.04.2015.
@@ -17,17 +19,17 @@ public class GenerateYAML {
         // Generate array
         //Sets the range for the random number
         int min = 0;
-        int maxCountOfNumbers = 1000000;
+        int maxCountOfNumbers = 500000; //800 000 generate about 3 minutes
         int max = Integer.MAX_VALUE / 2;
 
         int countOfNumbers = min + (int) (Math.random() * ((maxCountOfNumbers - min) + 1));
-        // We creat array with even count of Numbers. All numbers have a pair. then we remove one.
+        // We create array with even count of Numbers. All numbers have a pair. then we remove one.
         if (countOfNumbers % 2 != 0) {
             countOfNumbers = countOfNumbers + 1;
         }
         System.out.println(countOfNumbers);
-        // I need to remove one element, so i use ArrayList, not array.
-        ArrayList<Integer> array = new ArrayList<Integer>(countOfNumbers);
+        // I need to remove one element and add every element in random position, so i use ArrayList, not array.
+       List<Integer> array = new ArrayList<>(countOfNumbers);
         //fill an array with random numbers
         for (int i = 0; i < countOfNumbers; i = i + 2) {
             int newNumber = min + (int) (Math.random() * ((max - min) + 1));
@@ -39,7 +41,7 @@ public class GenerateYAML {
         int positionForRemove = 0 + (int) (Math.random() * (((array.size() - 1) - 0) + 1));
         array.remove(positionForRemove);
         // create array
-        int[] arrayToFile = new int[countOfNumbers - 1];
+        int[] arrayToFile =  new int[countOfNumbers - 1];
         int contIter = 0;
         for (Integer res : array) {
             arrayToFile[contIter++] = res.intValue();
